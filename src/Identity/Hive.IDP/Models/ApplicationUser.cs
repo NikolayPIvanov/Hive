30882316164777daae14abba072c1fffbaf1bf88
Nikolay Ivanov <nikolay.ivanov@scalefocus.com>
2021-01-21 17:44:37 +0200

    Adding IDP and user registration

---
+++ src/Identity/Hive.IDP/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hive.IDP.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public bool IsAdmin { get; set; }
+        public string DataEventRecordsRole { get; set; }
+        public string SecuredFilesRole { get; set; }
+        public DateTime AccountExpires { get; set; }
+    }
+}
