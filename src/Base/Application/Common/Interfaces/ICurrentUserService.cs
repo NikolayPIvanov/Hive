2550a438acdc216ed5c7827bfdcb5a7dbc7dce8e
Nikolay Ivanov <nikolay.ivanov@scalefocus.com>
2021-02-20 18:50:17 +0200

    Adding seller entities

---
+++ src/Base/Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace Hive.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+    }
+}
---
+++ src/Seller/Seller.Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Hive.Seller.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hive.Seller.Infrastructure.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Title).HasMaxLength(50).IsRequired();
+        }
+    }
+}
