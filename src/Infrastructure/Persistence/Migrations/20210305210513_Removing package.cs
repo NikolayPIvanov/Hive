68954f2b9b25b2614fc32ab27e00151218231235
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-06 13:41:20 +0200

    Adding domain models and migrations

---
+++ src/Infrastructure/Persistence/Migrations/20210305210513_Removing package.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hive.Infrastructure.Persistence.Migrations
+{
+    public partial class Removingpackage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Packages_Gigs_GigId",
+                table: "Packages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Packages",
+                table: "Packages");
+
+            migrationBuilder.RenameTable(
+                name: "Packages",
+                newName: "Package");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Packages_GigId",
+                table: "Package",
+                newName: "IX_Package_GigId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Package",
+                table: "Package",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Package_Gigs_GigId",
+                table: "Package",
+                column: "GigId",
+                principalTable: "Gigs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Package_Gigs_GigId",
+                table: "Package");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Package",
+                table: "Package");
+
+            migrationBuilder.RenameTable(
+                name: "Package",
+                newName: "Packages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Package_GigId",
+                table: "Packages",
+                newName: "IX_Packages_GigId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Packages",
+                table: "Packages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Packages_Gigs_GigId",
+                table: "Packages",
+                column: "GigId",
+                principalTable: "Gigs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20210306095449_Adding user profile.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hive.Infrastructure.Persistence.Migrations
+{
+    public partial class Addinguserprofile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SellerId",
+                table: "Gigs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Languages = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Skills = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Education = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SellerId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDraft = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    UserProfileId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sellers_UserProfiles_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gigs_SellerId",
+                table: "Gigs",
+                column: "SellerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sellers_UserProfileId",
+                table: "Sellers",
+                column: "UserProfileId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gigs_Sellers_SellerId",
+                table: "Gigs",
+                column: "SellerId",
+                principalTable: "Sellers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gigs_Sellers_SellerId",
+                table: "Gigs");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Gigs_SellerId",
+                table: "Gigs");
+
+            migrationBuilder.DropColumn(
+                name: "SellerId",
+                table: "Gigs");
+        }
+    }
+}
