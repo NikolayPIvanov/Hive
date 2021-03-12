14dba79c579a1d35a96dc372e39f2a09650a74a1
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-12 09:08:14 +0200

    Adding Get seller orders functionality

---
+++ src/Application/Orders/Queries/GetSellerOrders/GetSellerOrdersQuery.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Hive.Application.Common.Interfaces;
+using Hive.Application.Common.Mappings;
+using Hive.Domain.Enums;
+using MediatR;
+
+namespace Hive.Application.Orders.Queries.GetSellerOrders
+{
+    public static class GetSellerOrdersQuery
+    {
+        public record Query(int SellerId, OrderStatus? Status = null) : IRequest<IEnumerable<OrderDto>>;
+        
+        public class Handler : IRequestHandler<Query, IEnumerable<OrderDto>>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            
+            public async Task<IEnumerable<OrderDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var query = _context.Orders.Where(o => o.OfferedById == request.SellerId);
+
+                if (request.Status.HasValue)
+                {
+                    query = query.Where(o => o.Status == request.Status.Value);
+                }
+
+                var orders = await query.ProjectToListAsync<OrderDto>(_mapper.ConfigurationProvider);
+
+                return orders;
+            }
+        }
+    }
+}
---
+++ src/Application/Orders/Queries/GetSellerOrders/OrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Hive.Application.Common.Mappings;
+using Hive.Domain.Entities.Orders;
+using Hive.Domain.Enums;
+
+namespace Hive.Application.Orders.Queries.GetSellerOrders
+{
+    public class OrderDto : IMapFrom<Order>
+    {
+        public Guid OrderNumber { get; set; }
+        
+        public DateTime OrderedAt { get; private init; }
+        
+        public OrderStatus Status { get; set; }
+
+        public decimal TotalAmount { get; set; }
+        
+        public int GigId { get; set; }
+        
+        public int PackageId { get; set; }
+    }
+}
