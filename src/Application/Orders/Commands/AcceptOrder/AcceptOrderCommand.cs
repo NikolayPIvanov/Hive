fb78000cdb4ff1ba301a6bec56bdd843e48ba537
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-12 23:39:48 +0200

    Accept order command

---
+++ src/Application/Orders/Commands/AcceptOrder/AcceptOrderCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Hive.Application.Orders.Commands.AcceptOrder
+{
+    public class AcceptOrderCommand : IRequest
+    {
+        public Guid OrderNumber { get; set; }
+    }
+    
+    public class AcceptOrderCommandHandler : IRequestHandler<AcceptOrderCommand>
+    {
+        public Task<Unit> Handle(AcceptOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
