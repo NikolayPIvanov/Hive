853dd70d51bb8fcfa25bc6f56234f483408c179b
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-20 16:51:54 +0200

    Adding delete category

---
+++ src/GigManagement/Gig.Application/Categories/Queries/CategoriesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Gig.Contracts;
+using Hive.Gig.Domain.Entities;
+
+namespace Hive.Gig.Application.Categories.Queries
+{
+    public class CategoriesProfile : Profile
+    {
+        public CategoriesProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+        }
+    }
+}
