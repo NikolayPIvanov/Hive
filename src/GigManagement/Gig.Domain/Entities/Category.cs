15cfe55309d44d5d393bdcf0c08b06aba05a14c1
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-20 11:50:21 +0200

    Creating base gig entity

---
+++ src/GigManagement/Gig.Domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Hive.Common.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public int Id { get; set; }
+        
+        
+    }
+}
---
+++ src/GigManagement/Gig.Domain/Entities/Gig.cs
@@ -0,0 +1,16 @@
+﻿namespace Hive.Common.Domain.Entities
+{
+    public class Gig : AuditableEntity
+    {
+        // Numeric Ids take less space in tables
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        
+        
+        public int CategoryId { get; set; }
+        // TODO: Add Navigational Property if needed
+    }
+}
