3c5ca089b8d92210e0276f3267447d16db8e38be
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-24 11:15:29 +0200

    Adding set in progress functionality

--- src/Ordering/Ordering.Application/Orders/Commands/SetInProgressOrderCommand.cs
+++ src/Ordering/Ordering.Application/Orders/Commands/SetInProgressOrderCommand.cs
@@ -6,23 +6,21 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Ordering.Application.Interfaces;
 using Ordering.Domain.Entities;
-using OrderStatus = Ordering.Domain.Enums.OrderStatus;
 
 namespace Ordering.Application.Orders.Commands
 {
-    // todo: might merge to one action
-    public record DeclineOrderCommand(Guid OrderNumber) : IRequest;
+    public record SetInProgressOrderCommand(Guid OrderNumber) : IRequest;
     
-    public class DeclineOrderCommandHandler : IRequestHandler<DeclineOrderCommand>
+    public class SetInProgressOrderCommandHandler : IRequestHandler<SetInProgressOrderCommand>
     {
         private readonly IOrderingContext _context;
 
-        public DeclineOrderCommandHandler(IOrderingContext context)
+        public SetInProgressOrderCommandHandler(IOrderingContext context)
         {
             _context = context;
         }
         
-        public async Task<Unit> Handle(DeclineOrderCommand request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(SetInProgressOrderCommand request, CancellationToken cancellationToken)
         {
             var order = await _context.Orders
                 .Include(o => o.Status)
@@ -33,8 +31,8 @@ namespace Ordering.Application.Orders.Commands
                 throw new NotFoundException(nameof(Order), request.OrderNumber);
             }
             
-            order.Status.Status = OrderStatus.Declined;
-            order.Status.Reason = "Order declined by seller";
+            order.Status.Status = Domain.Enums.OrderStatus.InProgress;
+            order.Status.Reason = "Order marked in progress";
 
             await _context.SaveChangesAsync(cancellationToken);
             return Unit.Value;
