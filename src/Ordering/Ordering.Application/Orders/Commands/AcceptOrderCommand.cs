74d5b3f38c482cc3c3eca82b770768345f0349f2
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-24 10:39:43 +0200

    Adding accept and decline functionality

---
+++ src/Ordering/Ordering.Application/Orders/Commands/AcceptOrderCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hive.Common.Application.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.Entities;
+using OrderStatus = Ordering.Domain.Enums.OrderStatus;
+
+namespace Ordering.Application.Orders.Commands
+{
+    public record AcceptOrderCommand(Guid OrderNumber) : IRequest;
+    
+    public class AcceptOrderCommandHandler : IRequestHandler<AcceptOrderCommand>
+    {
+        private readonly IOrderingContext _context;
+
+        public AcceptOrderCommandHandler(IOrderingContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Unit> Handle(AcceptOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _context.Orders
+                .Include(o => o.Status)
+                .FirstOrDefaultAsync(o => o.OrderNumber == request.OrderNumber, cancellationToken: cancellationToken);
+
+            if (order is null)
+            {
+                throw new NotFoundException(nameof(Order), request.OrderNumber);
+            }
+            
+            order.Status.Status = OrderStatus.Accepted;
+            order.Status.Reason = "Order accepted by seller";
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Ordering/Ordering.Application/Orders/Commands/DeclineOrderCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hive.Common.Application.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.Entities;
+using OrderStatus = Ordering.Domain.Enums.OrderStatus;
+
+namespace Ordering.Application.Orders.Commands
+{
+    // todo: might merge to one action
+    public record DeclineOrderCommand(Guid OrderNumber) : IRequest;
+    
+    public class DeclineOrderCommandHandler : IRequestHandler<DeclineOrderCommand>
+    {
+        private readonly IOrderingContext _context;
+
+        public DeclineOrderCommandHandler(IOrderingContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Unit> Handle(DeclineOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = await _context.Orders
+                .Include(o => o.Status)
+                .FirstOrDefaultAsync(o => o.OrderNumber == request.OrderNumber, cancellationToken: cancellationToken);
+
+            if (order is null)
+            {
+                throw new NotFoundException(nameof(Order), request.OrderNumber);
+            }
+            
+            order.Status.Status = OrderStatus.Declined;
+            order.Status.Reason = "Order accepted by seller";
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
