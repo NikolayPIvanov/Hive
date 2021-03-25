a2d37b558ca5fa5bd6352fcb928d2cfe2a4bd319
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-25 09:19:18 +0200

    Adding IBillingContext.cs

---
+++ src/Billing/Billing.Application/Interfaces/IBillingContext.cs
@@ -0,0 +1,14 @@
+﻿using Billing.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Billing.Application.Interfaces
+{
+    public interface IBillingContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    }
+}
