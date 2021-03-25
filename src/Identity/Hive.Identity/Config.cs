dba9b65cece721fa9f019cd0dbe011b714992fa4
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-25 19:37:28 +0200

    wip

--- src/Identity/Hive.Identity/Config.cs
+++ src/Identity/Hive.Identity/Config.cs
@@ -21,6 +21,7 @@ namespace Hive.Identity
             {
                 new ApiScope("scope1"),
                 new ApiScope("scope2"),
+                new ApiScope("loosely-coupled"),
             };
 
         public static IEnumerable<Client> Clients =>
@@ -35,7 +36,7 @@ namespace Hive.Identity
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     ClientSecrets = {new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256())},
 
-                    AllowedScopes = {"scope1"}
+                    AllowedScopes = {"scope1", "loosely-coupled" }
                 },
 
                 // interactive client using code flow + pkce
