1dc398ff99ebca1ef7955e2da85a0748ed9e73f1
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-25 09:24:23 +0200

    Adding Billing Infrastructure

---
+++ src/Billing/Billing.Infrastructure/BillingContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Billing.Application.Interfaces;
+using Billing.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Billing.Infrastructure
+{
+    public class BillingContext : DbContext, IBillingContext
+    {
+        private const string DefaultSchema = "billing";
+        
+        public BillingContext(
+            DbContextOptions<BillingContext> options) : base(options)
+        {
+        }
+
+        public string Schema => DefaultSchema;
+        
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.HasDefaultSchema(DefaultSchema);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/Billing/Billing.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,32 @@
+﻿using Billing.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Billing.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddBilling(this IServiceCollection services, IConfiguration configuration)
+        {
+            var useInMemory = configuration.GetValue<bool>("UseInMemoryDatabase");
+            var sqlServerConnectionString = configuration.GetConnectionString("DefaultConnection");
+            if (useInMemory)
+            {
+                services.AddDbContext<BillingContext>(options =>
+                    options.UseInMemoryDatabase("LooselyHive"));
+            }
+            else
+            {
+                services.AddDbContext<BillingContext>(options =>
+                    options.UseSqlServer(
+                        sqlServerConnectionString,
+                        b => b.MigrationsAssembly(typeof(BillingContext).Assembly.FullName)));
+            }
+
+            services.AddScoped<IBillingContext>(provider => provider.GetService<BillingContext>());
+
+            return services;
+        }
+    }
+}
