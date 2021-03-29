5f0a0c01630c523265b4f3d1666058709cbd2627
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-29 16:27:23 +0300

    Adding domain event for verification of order data

--- src/Ordering/Ordering.Application/IntegrationEvents/EventHandlers/GigManagement/OrderValidatedIntegrationEventHandler.cs
+++ src/Ordering/Ordering.Application/IntegrationEvents/EventHandlers/GigManagement/OrderValidatedIntegrationEventHandler.cs
@@ -2,8 +2,10 @@
 using System.Threading.Tasks;
 using DotNetCore.CAP;
 using Hive.Gig.Contracts.IntegrationEvents;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Ordering.Application.Interfaces;
+using Ordering.Application.Orders.EventHandlers;
 using Ordering.Domain.Entities;
 using Ordering.Domain.Enums;
 
@@ -12,21 +14,29 @@ namespace Ordering.Application.IntegrationEvents.EventHandlers.GigManagement
     public class OrderValidatedIntegrationEventHandler : ICapSubscribe
     {
         private readonly IOrderingContext _context;
+        private readonly IMediator _mediator;
 
-        public OrderValidatedIntegrationEventHandler(IOrderingContext context)
+        public OrderValidatedIntegrationEventHandler(IOrderingContext context, IMediator mediator)
         {
             _context = context;
+            _mediator = mediator;
         }
         
         [CapSubscribe(nameof(OrderValidatedIntegrationEvent), Group = "cap.hive.ordering")] 
         public async Task Handle(OrderValidatedIntegrationEvent @event)
         {
             var order = await _context.Orders
-                .Select(o => new { o.Id, o.OrderNumber })
+                .Select(o => new { o.Id, o.OrderNumber, o.UnitPrice, UserId = o.Buyer.UserId})
                 .FirstOrDefaultAsync(o => o.OrderNumber == @event.OrderNumber);
 
             var orderState = @event.IsValid ? OrderState.OrderValid : OrderState.Invalid;
             var state = new State(orderState, @event.Reason, order.Id);
+
+            if (orderState == OrderState.OrderValid)
+            {
+                await _mediator.Publish(new OrderValidatedEvent(@event.OrderNumber, order.UnitPrice, order.UserId));
+            }
+
             _context.OrderStates.Add(state);
             await _context.SaveChangesAsync(default);
         }
