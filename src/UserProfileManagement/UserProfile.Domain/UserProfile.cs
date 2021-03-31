698bde7791529a4e1f10b5c4d07e7afa19e346b8
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-03-31 08:41:15 +0300

    Adding user profile domain

---
+++ src/UserProfileManagement/UserProfile.Domain/UserProfile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Hive.Common.Domain;
+
+namespace UserProfile.Domain
+{
+    public record Language(string Value);
+
+    public record Skill(string Value);
+    
+    public class UserProfile : AuditableEntity
+    {
+        private UserProfile()
+        {
+            Languages = new HashSet<Language>(5);
+            Skills = new HashSet<Skill>(5);
+        }
+        
+        // TODO
+        public UserProfile(string userId, string firstName, string lastName)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        
+        public int Id { get; set; }
+        
+        public string UserId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string? Description { get; set; }
+        
+        public string? Education { get; set; }
+        
+        public ICollection<Skill> Skills { get; set; }
+        public ICollection<Language> Languages { get; set; }
+    }
+}
