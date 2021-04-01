e131b6dc3b32539b1c78b39894320afea86cec9b
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-04-01 19:01:02 +0300

    Adding plan delete plan command and plan get by vendor query

---
+++ src/Investing/Investing.Application/Plans/Commands/CreatePlanCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hive.Investing.Application.Interfaces;
+using Hive.Investing.Domain.Entities;
+using MediatR;
+
+namespace Hive.Investing.Application.Plans.Commands
+{
+    public record CreatePlanCommand(int VendorId, string Title, string Description,
+        int EstimatedReleaseDays, DateTime? EstimatedReleaseDate, decimal FundingNeeded) : IRequest<int>;
+    
+    public class CreatePlanCommandHandler : IRequestHandler<CreatePlanCommand, int>
+    {
+        private readonly IInvestingDbContext _context;
+
+        public CreatePlanCommandHandler(IInvestingDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<int> Handle(CreatePlanCommand request, CancellationToken cancellationToken)
+        {
+            var plan = new Plan(request.VendorId, request.Title, request.Description, request.EstimatedReleaseDays,
+                request.EstimatedReleaseDate, request.FundingNeeded);
+            // TODO: Add validation
+            _context.Plans.Add(plan);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return plan.Id;
+        }
+    }
+}
---
+++ src/Investing/Investing.Application/Plans/Queries/GetPlansForVendorQuery.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Hive.Common.Application.Mappings;
+using Hive.Investing.Application.Interfaces;
+using MediatR;
+
+namespace Hive.Investing.Application.Plans.Queries
+{
+    // TODO: describe dto
+    public record PlanDto;
+
+    public record GetPlansForVendorQuery(int VendorId) : IRequest<IEnumerable<PlanDto>>;
+
+    public class GetPlansForVendorQueryHandler : IRequestHandler<GetPlansForVendorQuery, IEnumerable<PlanDto>>
+    {
+        private readonly IInvestingDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPlansForVendorQueryHandler(IInvestingDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<IEnumerable<PlanDto>> Handle(GetPlansForVendorQuery request, CancellationToken cancellationToken)
+        {
+            // validate for authorization
+            var currentUserId = "str";
+            return await _context.Plans
+                .Where(p => p.VendorId == request.VendorId)
+                .ProjectToListAsync<PlanDto>(_mapper.ConfigurationProvider);
+        }
+    }
+}
