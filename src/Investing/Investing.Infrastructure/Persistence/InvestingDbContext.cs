a6cdd8dfca2295dcdbda2a18b18e5a9d04ee5eea
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-04-01 17:38:48 +0300

    Adding investing infrastructure

---
+++ src/Investing/Investing.Infrastructure/Persistence/InvestingDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Hive.Common.Application.Interfaces;
+using Hive.Investing.Application.Interfaces;
+using Hive.Investing.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hive.Investing.Infrastructure.Persistence
+{
+    public class InvestingDbContext : DbContext, IInvestingDbContext
+    {
+        private readonly IDateTimeService _dateTimeService;
+        private const string DefaultSchema = "investing";
+        
+        public InvestingDbContext(
+            DbContextOptions<InvestingDbContext> options,
+            IDateTimeService dateTimeService) : base(options)
+        {
+            _dateTimeService = dateTimeService;
+        }
+
+        public static string Schema => DefaultSchema;
+        
+        public DbSet<Investor> Investors { get; set; }
+        public DbSet<Investment> Investments { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+    }
+}
