0a70ab7af95aaf37d033170fb9ac3a0a13ae2cb8
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-04-01 18:16:14 +0300

    Adding vendor and investor integration event handlers

---
+++ src/Investing/Investing.Application/IntegrationEvents/EventHandlers/Identity/InvestorCreatedIntegrationEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using DotNetCore.CAP;
+using Hive.Identity.Contracts.IntegrationEvents;
+using Hive.Investing.Application.Interfaces;
+using Hive.Investing.Domain.Entities;
+
+namespace Hive.Investing.Application.IntegrationEvents.EventHandlers.Identity
+{
+    public class InvestorCreatedIntegrationEventHandler : ICapSubscribe
+    {
+        private readonly IInvestingDbContext _context;
+
+        public InvestorCreatedIntegrationEventHandler(IInvestingDbContext context)
+        {
+            _context = context;
+        }
+        
+        [CapSubscribe(nameof(InvestorCreatedIntegrationEvent))]
+        public async Task Handle(InvestorCreatedIntegrationEvent @event)
+        {
+            var investor = new Investor(@event.UserId);
+
+            _context.Investors.Add(investor);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
