ed057529f4c301fb49be05cd6bcb1217cc94f6e2
Nikolay Ivanov <nick.ivanov98@gmail.com>
2021-04-29 08:32:18 +0300

    Adding redis settings

---
+++ src/BuildingBlocks/BuildingBlocks.Core/Caching/RedisExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Newtonsoft;
+
+namespace BuildingBlocks.Core.Caching
+{
+    public static class RedisExtension
+    {
+        public static IServiceCollection AddRedis(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(provider => 
+                configuration.GetSection("Redis").Get<RedisConfiguration>());
+
+            return services;
+        }
+    }
+}
