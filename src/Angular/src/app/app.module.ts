1b766421d044e63599d254f0a78bd07585f07b05
nikolay.ivanov@scalefocus.com <nikolay.ivanov@scalefocus.com>
2021-05-29 09:27:51 +0300

    wip

--- src/Angular/src/app/app.module.ts
+++ src/Angular/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { FuseModule } from '@fuse/fuse.module';
@@ -15,31 +14,14 @@ import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from
 
 import { fuseConfig } from 'app/fuse-config';
 
-import { FakeDbService } from 'app/fake-db/fake-db.service';
 import { AppComponent } from 'app/app.component';
-import { AppStoreModule } from 'app/store/store.module';
 import { LayoutModule } from 'app/layout/layout.module';
+import { SampleModule } from 'app/main/sample/sample.module';
 
 const appRoutes: Routes = [
     {
-        path        : 'apps',
-        loadChildren: () => import('./main/apps/apps.module').then(m => m.AppsModule)
-    },
-    {
-        path        : 'pages',
-        loadChildren: () => import('./main/pages/pages.module').then(m => m.PagesModule)
-    },
-    {
-        path        : 'ui',
-        loadChildren: () => import('./main/ui/ui.module').then(m => m.UIModule)
-    },
-    {
-        path        : 'documentation',
-        loadChildren: () => import('./main/documentation/documentation.module').then(m => m.DocumentationModule)
-    },
-    {
         path      : '**',
-        redirectTo: 'apps/dashboards/analytics'
+        redirectTo: 'sample'
     }
 ];
 
@@ -54,10 +36,6 @@ const appRoutes: Routes = [
         RouterModule.forRoot(appRoutes),
 
         TranslateModule.forRoot(),
-        InMemoryWebApiModule.forRoot(FakeDbService, {
-            delay             : 0,
-            passThruUnknownUrl: true
-        }),
 
         // Material moment date module
         MatMomentDateModule,
@@ -75,7 +53,7 @@ const appRoutes: Routes = [
 
         // App modules
         LayoutModule,
-        AppStoreModule
+        SampleModule
     ],
     bootstrap   : [
         AppComponent
