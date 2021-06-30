c46c74185d3cf9ad8a523a0a4289e6f4ad1e9a87
nikolay.ivanov@scalefocus.com <nikolay.ivanov@scalefocus.com>
2021-06-30 09:15:39 +0300

    buyer dashboard

--- src/Angular/hive/src/app/modules/home/home.module.ts
+++ src/Angular/hive/src/app/modules/home/home.module.ts
@@ -6,12 +6,14 @@ import { LayoutModule } from '../layout/layout.module';
 import { CoreModule } from '@angular/flex-layout';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { SellerDashboardComponent } from './components/seller-dashboard/seller-dashboard.component';
+import { BuyerDashboardComponent } from './components/buyer-dashboard/buyer-dashboard.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     AdminDashboardComponent,
-    SellerDashboardComponent
+    SellerDashboardComponent,
+    BuyerDashboardComponent
   ],
   imports: [
     CommonModule,
