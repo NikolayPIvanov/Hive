0f5b90eec0ad5cf4d65e84a4b836bbd543b3a208
nikolay.ivanov@scalefocus.com <nikolay.ivanov@scalefocus.com>
2021-07-06 11:44:20 +0300

    updating admin categories

--- src/Angular/hive/src/app/modules/category/components/categories-dashboard/categories-dashboard.component.ts
+++ src/Angular/hive/src/app/modules/category/components/categories-dashboard/categories-dashboard.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CategoriesClient, CategoriesType, CategoryDto, PaginatedListOfCategoryDto } from 'src/app/clients/gigs-client';
 import { CategoryDetailsComponent } from '../category-details/category-details.component';
 
@@ -12,16 +15,20 @@ import { CategoryDetailsComponent } from '../category-details/category-details.c
   templateUrl: './categories-dashboard.component.html',
   styleUrls: ['./categories-dashboard.component.scss']
 })
-export class CategoriesDashboardComponent implements OnInit {
+export class CategoriesDashboardComponent implements OnInit, OnDestroy {
+  private unsubscribe = new Subject();
+
   pageNumber = 0;
   pageSize = 10;
-  pageOptions = [5, 10, 25];
+  pageOptions = [10, 25, 50];
   length = 0;
   searchKey: string | null = null;
 
   private categoriesSubject = new BehaviorSubject<PaginatedListOfCategoryDto | undefined>(undefined);
   public categories$ = this.categoriesSubject.asObservable();
   public categories: CategoryDto[] = [];
+  public dataSource!: MatTableDataSource<CategoryDto>;
+  displayedColumns: string[] = ['title', 'description', 'parent', 'children', 'actions'];
 
   constructor(
     public dialog: MatDialog,
@@ -29,12 +36,25 @@ export class CategoriesDashboardComponent implements OnInit {
     private categoriesApiClient: CategoriesClient) { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<CategoryDto>([])
     this.fetchCategories()
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  setPage(e: PageEvent) {
+    this.pageNumber = e.pageIndex;
+    this.pageSize = e.pageSize;
+    this.fetchCategories();
   }
 
   selected(category: CategoryDto) {
     if (category) {
       this.categories = [category]
+      this.dataSource.data = [category];
     }
     else {
       this.fetchCategories();
@@ -44,10 +64,12 @@ export class CategoriesDashboardComponent implements OnInit {
     this.categoriesApiClient.deleteCategory(category.id!)
       .pipe(tap({
         next: () => {
-          const index = this.categories.indexOf(category);
+          const copy = this.dataSource.data
+          const index = copy.indexOf(category);
           if (index > -1) {
-            this.categories.splice(index, 1);
+            copy.splice(index, 1);
           }
+          this.dataSource.data = copy;
       }}))
       .subscribe()
   }
@@ -62,9 +84,11 @@ export class CategoriesDashboardComponent implements OnInit {
     dialogRef.afterClosed()
       .pipe(tap({
         next: (updatedCategory) => {
-          const index = this.categories.indexOf(category);
+          const copy = this.dataSource.data;
+          const index = copy.indexOf(category);
           if (updatedCategory && index > -1) {
-            this.categories.splice(index, 1, updatedCategory);
+            copy.splice(index, 1, updatedCategory);
+            this.dataSource.data = copy;
           }
         }
       }))
@@ -73,6 +97,11 @@ export class CategoriesDashboardComponent implements OnInit {
 
   onClosedDialog(category: CategoryDto) {
     this.categories.push(category)
+
+    const copy = this.dataSource.data
+    copy.push(category);
+    this.dataSource.data = copy;
+
     this.length += 1;
   }
 
@@ -88,6 +117,7 @@ export class CategoriesDashboardComponent implements OnInit {
             this.categoriesSubject.next(list);
             this.categories = list.items!
             this.length = list.totalCount!;
+            this.dataSource.data = list.items!;
           },
           complete: () => { this.spinner.hide() }
         }
